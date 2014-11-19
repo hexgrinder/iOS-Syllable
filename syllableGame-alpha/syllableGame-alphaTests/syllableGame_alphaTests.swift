20555de0e9e5113ef43247b720356b9ed8eb7e64
cisstudents <cisstudents@ATC203-M25.local>
2014-11-19 12:43:45 -0800

    init commit

---
+++ syllableGame-alpha/syllableGame-alphaTests/syllableGame_alphaTests.swift
@@ -0,0 +1,36 @@
+//
+//  syllableGame_alphaTests.swift
+//  syllableGame-alphaTests
+//
+//  Created by cisstudents on 11/19/14.
+//  Copyright (c) 2014 hexgrinder. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class syllableGame_alphaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
