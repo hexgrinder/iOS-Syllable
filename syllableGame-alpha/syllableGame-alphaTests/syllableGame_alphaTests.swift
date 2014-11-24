faec7c1b61af0540d7d462dd4b06430fde0583d8
cisstudents <cisstudents@ATC203-M25.local>
2014-11-24 15:31:22 -0800

    test commit

--- syllableGame-alpha/syllableGame-alphaTests/syllableGame_alphaTests.swift
+++ syllableGame-alpha/syllableGame-alphaTests/syllableGame_alphaTests.swift
@@ -14,6 +14,7 @@ class syllableGame_alphaTests: XCTestCase {
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        println("I like cats")
     }
     
     override func tearDown() {
